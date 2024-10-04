de7fad20d836b9416030774adf03ecc5dd05e774
thainq <thainguyen12.work@gmail.com>
2024-10-04 14:32:10 +0700

    clean web page and change authen to root

--- app/information/page.tsx
+++
@@ -1,6 +0,0 @@
-import { useFormContext, useWatch } from "react-hook-form";
-import { AuthenticatorSchema } from "../authentication/type";
-
-export default function InformationPage() {
-  return <></>;
-}
